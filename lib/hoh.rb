43194fba8208691ce4f53fe96612232a6df6aa03
Daniel Kersten <dkersten1994@gmail.com>
2020-03-10 00:16:15 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,28 +43,7 @@ def naming_system
   # HoH all the way to Species!
   {
     
-    :PORTION_1 => {
-      label: "Kingdom",
-      sub_category: {
-        label: "Phylum",
-        sub_category: {
-          label: "Class"
-        }
-      }
-    },
-    :PORTION_2 => {
-      label: "Order"
-    },
-    :PORTION_3 => {
-      label: "Family",
-      sub_category: {
-        label: "Genus",
-      }
-    },
-    :PORTION_4 => {
-      label: "Species",
-      sub_category: nil
-    }
+    label: Kingdom
     
   }
 end
